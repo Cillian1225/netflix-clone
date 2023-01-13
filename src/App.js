cc68001b702242e08cac8ba6e61d8cd1a7aa05ca
Cillian Jung93 <86462224+Cillian1225@users.noreply.github.com>
2023-01-13 18:18:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+      ;
+    </div>
+  );
+}
+
+export default App;
